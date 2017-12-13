5d10233730c83783c57cc57fbb39264e843668b2
Matt Sewell <matt@pythonfinancial.com>
2017-12-13 02:41:16 +0000

    initial commit

---
+++ musicians.py
@@ -0,0 +1,42 @@
+class Musician(object):
+    def __init__(self, sounds):
+        self.sounds = sounds
+
+    def solo(self, length):
+        for i in range(length):
+            print(self.sounds[i % len(self.sounds)], end=" ")
+        print()
+
+class Bassist(Musician): # The Musician class is the parent of the Bassist class
+    def __init__(self):
+        # Call the __init__ method of the parent class
+        super().__init__(["Twang", "Thrumb", "Bling"])
+
+class Guitarist(Musician):
+    def __init__(self):
+        # Call the __init__ method of the parent class
+        super().__init__(["Boink", "Bow", "Boom"])
+
+    def tune(self):
+        print("Be with you in a moment")
+        print("Twoning, sproing, splang")
+        
+class Drummer(Musician):
+    def __init__(self):
+        # Call the __init__ method of the parent class
+        super().__init__(["Bang", "Tink", "Tsshh"])
+        
+    def four_count(self):
+        print("One, Two, Three, Four")
+    
+    def combust(self):
+        print("Oh no! I spontaneously combusted")
+    
+    
+class Bands(object):
+    def __init__(self, hire, fire):
+        self.hire = hire
+        self.fire = fire
+    #hire, fire, play solo after drummer has counted time
+    
+    
